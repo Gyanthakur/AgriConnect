45dc4ae215c2905df1ff46b71808f960e4db52b2
Gyanthakur <gps.96169@gmail.com>
2024-11-14 20:46:26 +0530

    Feat: added market Price

--- client/src/components/MarketPrice.jsx
+++ client/src/components/MarketPrice.jsx
@@ -14,9 +14,11 @@ const MarketPrice = () => {
     "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", 
     "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", 
     "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", 
-    "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal", "Delhi", 
-    "Lakshadweep", "Puducherry", "Andaman and Nicobar Islands", "Chandigarh"
+    "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal", 
+    "Delhi", "Lakshadweep", "Puducherry", "Andaman and Nicobar Islands", "Chandigarh", 
+    "Dadra and Nagar Haveli and Daman and Diu", "Jammu and Kashmir", "Ladakh"
   ];
+  
 
   // Handle changes in the state selection
   const handleStateChange = (e) => {
