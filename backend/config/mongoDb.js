cfc0fff63df8b0c5780f68510684bbb3f390a193
Gyanthakur <gps.96169@gmail.com>
2025-02-03 17:08:10 +0530

    update: config

--- backend/config/mongoDb.js
+++ backend/config/mongoDb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDb = async () =>{
     mongoose.connection.on('connected',()=>console.log('Database successfully connected'))
-    await mongoose.connect(`${process.env.MONGO_Db_URI}/AgriConnect`)
+    await mongoose.connect(`${process.env.MONGO_DB_URI}/AgriConnect`)
 }
 
 export default connectDb;
