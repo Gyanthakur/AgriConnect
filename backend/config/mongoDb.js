0cd751d28eee8bc72295c3e0505d1e70a3139137
Gyanthakur <gps.96169@gmail.com>
2025-03-06 12:37:43 +0530

    feat: update mongodb

--- backend/config/mongoDb.js
+++ backend/config/mongoDb.js
@@ -6,34 +6,3 @@ const connectDb = async () =>{
 
 export default connectDb;
 
-// import mongoose from "mongoose";
-
-// let cachedDb = null;
-
-// const connectDb = async () => {
-//     if (cachedDb) {
-//         console.log('Using cached database instance');
-//         return cachedDb;
-//     }
-
-//     try {
-//         mongoose.connection.on('connected', () => console.log('Database successfully connected'));
-//         mongoose.connection.on('error', (err) => console.error(`Database connection error: ${err}`));
-//         // mongoose.connection.on('disconected', () => console.log('Database disconnected'));
-
-//         // const options = {
-//         //     useNewUrlParser: true,
-//         //     useUnifiedTopology: true,
-//         //     useCreateIndex: true,
-//         //     useFindAndModify: false,
-//         // };
-
-//         cachedDb = await mongoose.connect(`${process.env.MONGO_DB_URI}/AgriConnect`);
-//         return cachedDb;
-//     } catch (error) {
-//         console.error(`Initial database connection error: ${error}`);
-//         setTimeout(connectDb, 5000); // Retry connection after 5 seconds
-//     }
-// };
-
-// export default connectDb;
