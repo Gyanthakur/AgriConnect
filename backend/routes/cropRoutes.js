bf751801606a1a1069e713303838b81d94c49afa
Gyanthakur <gps.96169@gmail.com>
2025-03-26 01:15:10 +0530

    feat: added crop routes

---
+++ backend/routes/cropRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { addCrop, getAllCrops, getCropById, updateCrop, deleteCrop } from "../controllers/cropController.js";
+// import upload from "../moddlewares/multer.js";
+import upload from "../moddlewares/multer.js";
+import authFarmer from "../moddlewares/farmerUser.js";
+
+const cropRouter = express.Router();
+
+cropRouter.post("/add", authFarmer, upload.single("image"), addCrop);
+cropRouter.get("/all", getAllCrops);
+cropRouter.get("/:cropId", getCropById);
+cropRouter.put("/update/:cropId", authFarmer, upload.single("image"), updateCrop);
+cropRouter.delete("/delete/:cropId", authFarmer, deleteCrop);
+
+export default cropRouter;
--- backend/routes/farmerRoute.js
+++ backend/routes/farmerRoute.js
@@ -1,20 +1,28 @@
 import express from 'express'
-import {allFarmers, getprofile, LoginFarmer, paymentRazorpay, registerFarmer, updateProfile, verifyRazorpay } from '../controllers/farmerController.js'
+// import {addProduct, allFarmers, deleteProduct, getAllProducts, getProductById, getprofile, LoginFarmer,  registerFarmer, updateProduct, updateProfile } from '../controllers/farmerController.js'
+import {allFarmers, getProfile, LoginFarmer, paymentRazorpay, registerFarmer, updateProfile, verifyRazorpay } from '../controllers/farmerController.js'
 
 import upload from '../moddlewares/multer.js';
 import authFarmer from '../moddlewares/farmerUser.js';
+import cropRouter from './cropRoutes.js';
 
 const farmerRouter = express.Router();
 
 farmerRouter.post('/register',registerFarmer)
 farmerRouter.post('/login',LoginFarmer)
 
-farmerRouter.get('/get-profile',authFarmer,getprofile)
+farmerRouter.get('/get-profile',authFarmer,getProfile)
 farmerRouter.post('/update-profile',upload.single('image'),authFarmer,updateProfile)
 
-farmerRouter.post('/payment-razorpay',authFarmer,paymentRazorpay)
-farmerRouter.post('/verifyRazorpay',authFarmer,verifyRazorpay)
+
+
+// farmerRouter.post('/payment-razorpay',authFarmer,paymentRazorpay)
+// farmerRouter.post('/verifyRazorpay',authFarmer,verifyRazorpay)
 
 farmerRouter.get('/all-farmer',allFarmers)
+
+farmerRouter.use("/crop", cropRouter);
+
+
 
 export default farmerRouter;
