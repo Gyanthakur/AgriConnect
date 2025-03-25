ac6f59c355c262c01a9e07d4d297d1b2e25092cd
Gyanthakur <gps.96169@gmail.com>
2025-03-26 01:14:56 +0530

    feat: added crop model

---
+++ backend/models/cropModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const cropSchema = new mongoose.Schema({
+    farmerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Farmer",
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    status: {
+      type: String,
+      enum: ["available", "sold"],
+      default: "available", // Default status is available
+  },
+    image: {
+        type: String, // Cloudinary image URL
+        required: true
+    },
+}, { timestamps: true });
+
+const Crop = mongoose.model("Crop", cropSchema);
+export default Crop;
