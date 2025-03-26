6ae72fccc3e3e5fc5e32c1518c0d9d11db44ec43
Gyanthakur <gps.96169@gmail.com>
2025-03-26 15:14:25 +0530

    modified: crop routes

--- backend/routes/cropRoutes.js
+++ backend/routes/cropRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addCrop, getAllCrops, getCropById, updateCrop, deleteCrop } from "../controllers/cropController.js";
+import { addCrop, getAllCrops, getCropById, updateCrop, deleteCrop, getMyCrops } from "../controllers/cropController.js";
 // import upload from "../moddlewares/multer.js";
 import upload from "../moddlewares/multer.js";
 import authFarmer from "../moddlewares/farmerUser.js";
@@ -8,8 +8,10 @@ const cropRouter = express.Router();
 
 cropRouter.post("/add", authFarmer, upload.single("image"), addCrop);
 cropRouter.get("/all", getAllCrops);
+cropRouter.get("/mine",authFarmer, getMyCrops);
 cropRouter.get("/:cropId", getCropById);
 cropRouter.put("/update/:cropId", authFarmer, upload.single("image"), updateCrop);
 cropRouter.delete("/delete/:cropId", authFarmer, deleteCrop);
+
 
 export default cropRouter;
