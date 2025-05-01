51db74255b0336af1a47470f915007fc0fd71532
Gyanthakur <gps.96169@gmail.com>
2025-05-01 19:39:51 +0530

    fix: fixed register issue

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ import equipmentRouter from './routes/equipment.routes.js'
 const app = express()
 app.use(express.json())
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173',"https://agri-connect-nu.vercel.app"],
     credentials: true,
 };
 
